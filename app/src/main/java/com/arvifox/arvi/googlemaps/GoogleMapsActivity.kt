4db624b3c81f49b37be24ff5f6ca4c1d2a47c4bf
Arvi Fox <arvifox@hotmail.com>
2018-07-30 10:09:15 +0300

    geo marker in Apatity

--- app/src/main/java/com/arvifox/arvi/googlemaps/GoogleMapsActivity.kt
+++ app/src/main/java/com/arvifox/arvi/googlemaps/GoogleMapsActivity.kt
@@ -51,8 +51,8 @@ class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        val sydney = LatLng(67.57, 33.4)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Apatity"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
