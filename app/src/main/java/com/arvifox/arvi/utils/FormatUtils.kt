e3ed4a602e6a24ec9e3581281a87dc1ec38ca7a4
Arvi Fox <arvifox@hotmail.com>
2018-07-31 17:19:51 +0300

    ext function to convert InputStream to String

--- app/src/main/java/com/arvifox/arvi/utils/FormatUtils.kt
+++ app/src/main/java/com/arvifox/arvi/utils/FormatUtils.kt
@@ -1,8 +1,22 @@
 package com.arvifox.arvi.utils
 
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+
 object FormatUtils {
 
     fun Double.format(digits: Int) = java.lang.String.format("%.${digits}f", this)
 
     fun Float.format(digits: Int) = java.lang.String.format("%.${digits}f", this)
+
+    fun InputStream.takeString(): String {
+        val bar = ByteArrayOutputStream()
+        val byar = ByteArray(1024)
+        var len: Int = this.read(byar)
+        while (len != -1) {
+            bar.write(byar, 0, len)
+            len = this.read(byar)
+        }
+        return bar.toString("UTF-8")
+    }
 }
