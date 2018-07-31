468d4a42d81e8c80dac444afe02364ef251e428d
Arvi Fox <arvifox@hotmail.com>
2018-07-31 12:38:13 +0300

    get Auth Token - done

--- app/src/main/java/com/arvifox/arvi/google/arcorevision/OAuthTestActivity.kt
+++ app/src/main/java/com/arvifox/arvi/google/arcorevision/OAuthTestActivity.kt
@@ -1,8 +1,13 @@
 package com.arvifox.arvi.google.arcorevision
 
+import android.accounts.AccountManager
+import android.accounts.AccountManagerCallback
+import android.accounts.AccountManagerFuture
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.arvifox.arvi.R
@@ -11,6 +16,8 @@ import kotlinx.android.synthetic.main.activity_oauth_test.*
 import kotlinx.android.synthetic.main.app_bar_layout.*
 
 class OAuthTestActivity : AppCompatActivity() {
+
+    private lateinit var token: String
 
     companion object {
         fun newIntent(c: Context): Intent {
@@ -28,5 +35,49 @@ class OAuthTestActivity : AppCompatActivity() {
                     .setAction("Action", null).show()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        askToken()
+    }
+
+    private fun askToken() {
+        val am = AccountManager.get(this)
+        val opt = Bundle()
+        val acc = am.getAccountsByType("com.google")[0]
+        am.getAuthToken(acc, "Manage your tasks", opt, this, OnTokenAcquired(), handler)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 234) {
+            if (resultCode == Activity.RESULT_OK) {
+                askToken()
+            }
+        }
+    }
+
+    private val handler = Handler()
+
+    private fun myToken() {
+        tvOAuth.text = token
+    }
+
+    inner class OnTokenAcquired : AccountManagerCallback<Bundle> {
+        override fun run(future: AccountManagerFuture<Bundle>?) {
+            var intent: Intent? = null
+            if (future != null && future.result != null) {
+                val ob = future.result.get(AccountManager.KEY_INTENT)
+                if (ob != null) intent = ob as Intent
+            }
+            if (intent != null) {
+                startActivityForResult(intent, 234)
+                return
+            }
+            val b = future?.result
+            token = b?.getString(AccountManager.KEY_AUTHTOKEN) ?: ""
+            myToken()
+        }
     }
 }
