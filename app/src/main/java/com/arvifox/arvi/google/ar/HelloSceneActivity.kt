8aa754f3d2b3213885dd4008831ca734fef87c20
Arvi Fox <arvifox@hotmail.com>
2018-08-08 12:51:41 +0300

    arcore, sceneform, change color of material

--- app/src/main/java/com/arvifox/arvi/google/ar/HelloSceneActivity.kt
+++ app/src/main/java/com/arvifox/arvi/google/ar/HelloSceneActivity.kt
@@ -1,5 +1,6 @@
 package com.arvifox.arvi.google.ar
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -10,9 +11,12 @@ import android.view.MotionEvent
 import android.widget.Toast
 import com.arvifox.arvi.R
 import com.arvifox.arvi.google.utils.ArUtils
+import com.arvifox.arvi.utils.Logger
 import com.google.ar.core.HitResult
 import com.google.ar.core.Plane
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.Color
+import com.google.ar.sceneform.rendering.MaterialFactory
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
@@ -24,6 +28,8 @@ class HelloSceneActivity : AppCompatActivity() {
     private var arFragment: ArFragment? = null
     private var model: ModelRenderable? = null
 
+    private var flag: Boolean = false
+
     companion object {
         fun newIntent(c: Context): Intent {
             return Intent(c, HelloSceneActivity::class.java)
@@ -32,7 +38,7 @@ class HelloSceneActivity : AppCompatActivity() {
 
     // CompletableFuture requires api level 24
     // FutureReturnValueIgnored is not valid
-    @SuppressWarnings("AndroidApiChecker", "FutureReturnValueIgnored")
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (!ArUtils.checkIsSupportedDeviceOrFinish(this)) {
@@ -43,8 +49,6 @@ class HelloSceneActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
-
-        if (Build.VERSION.SDK_INT < 24) return
 
         // When you build a Renderable, Sceneform loads its resources in the background while returning
         // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
@@ -61,20 +65,28 @@ class HelloSceneActivity : AppCompatActivity() {
                 }
 
         arFragment?.setOnTapArPlaneListener { hitResult: HitResult, plane: Plane, motionEvent: MotionEvent ->
-            if (model == null) {
-                return@setOnTapArPlaneListener
+            Logger.d { "tap plane" }
+            if (!flag) {
+                if (model == null) {
+                    return@setOnTapArPlaneListener
+                }
+
+                // Create the Anchor.
+                val anchor = hitResult.createAnchor()
+                val anchorNode = AnchorNode(anchor)
+                anchorNode.setParent(arFragment?.arSceneView?.scene)
+
+                // Create the transformable andy and add it to the anchor.
+                val andy = TransformableNode(arFragment?.transformationSystem)
+                andy.setParent(anchorNode)
+                model?.material?.setFloat3("baseColor", Color(android.graphics.Color.GREEN))
+                andy.renderable = model
+                andy.select()
+                flag = true
+            } else {
+                Logger.d { "material size =" + model?.submeshCount }
+                model?.material?.setFloat3("baseColor", Color(android.graphics.Color.BLUE))
             }
-
-            // Create the Anchor.
-            val anchor = hitResult.createAnchor()
-            val anchorNode = AnchorNode(anchor)
-            anchorNode.setParent(arFragment?.arSceneView?.scene)
-
-            // Create the transformable andy and add it to the anchor.
-            val andy = TransformableNode(arFragment?.transformationSystem)
-            andy.setParent(anchorNode)
-            andy.renderable = model
-            andy.select()
         }
     }
 }
