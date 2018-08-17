2c4ee74d81ef73b91223addddf10b6ca188ffec3
Arvi Fox <arvifox@hotmail.com>
2018-08-17 13:51:00 +0300

    draw a cube

--- app/src/main/java/com/arvifox/arvi/google/ar/HelloSceneActivity.kt
+++ app/src/main/java/com/arvifox/arvi/google/ar/HelloSceneActivity.kt
@@ -3,7 +3,6 @@ package com.arvifox.arvi.google.ar
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
@@ -15,20 +14,18 @@ import com.arvifox.arvi.utils.Logger
 import com.google.ar.core.HitResult
 import com.google.ar.core.Plane
 import com.google.ar.sceneform.AnchorNode
-import com.google.ar.sceneform.rendering.Color
-import com.google.ar.sceneform.rendering.MaterialFactory
-import com.google.ar.sceneform.rendering.ModelRenderable
-import com.google.ar.sceneform.rendering.Texture
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.*
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.app_bar_layout.*
-import java.util.concurrent.CompletableFuture
 
 
 class HelloSceneActivity : AppCompatActivity() {
 
     private var arFragment: ArFragment? = null
     private var model: ModelRenderable? = null
+    private var m: Material? = null
 
     private var flag: Boolean = false
 
@@ -66,6 +63,8 @@ class HelloSceneActivity : AppCompatActivity() {
                     null
                 }
 
+        MaterialFactory.makeOpaqueWithColor(this, Color(android.graphics.Color.BLUE)).thenAccept { mat -> m = mat }
+
         arFragment?.setOnTapArPlaneListener { hitResult: HitResult, plane: Plane, motionEvent: MotionEvent ->
             Logger.d { "tap plane" }
             if (!flag) {
@@ -78,10 +77,15 @@ class HelloSceneActivity : AppCompatActivity() {
                 val anchorNode = AnchorNode(anchor)
                 anchorNode.setParent(arFragment?.arSceneView?.scene)
 
+                // create a cube
+//                val v = Vector3(hitResult.hitPose.tx(), hitResult.hitPose.ty(), hitResult.hitPose.tz())
+                val v = Vector3(0f, 0f, -1f)
+                model = ShapeFactory.makeCube(Vector3(0.2f, 0.2f, 0.2f), v, m)
+
                 // Create the transformable andy and add it to the anchor.
                 val andy = TransformableNode(arFragment?.transformationSystem)
                 andy.setParent(anchorNode)
-                model?.material?.setFloat3("baseColor", Color(android.graphics.Color.GREEN))
+//                model?.material?.setFloat3("baseColor", Color(android.graphics.Color.GREEN))
                 andy.renderable = model
                 andy.select()
                 flag = true
