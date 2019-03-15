e04757de2ee4551ac1b5897384174e359b9a28af
arvifox <arvifox@yahoo.com>
2019-03-15 12:00:49 +0300

    anima

--- app/src/main/java/com/arvifox/arvi/simplemisc/anim/AnimActivity.kt
+++ app/src/main/java/com/arvifox/arvi/simplemisc/anim/AnimActivity.kt
@@ -1,14 +1,20 @@
 package com.arvifox.arvi.simplemisc.anim
 
+import android.animation.AnimatorInflater
 import android.animation.ValueAnimator
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AccelerateDecelerateInterpolator
-import com.arvifox.arvi.R
 import kotlinx.android.synthetic.main.activity_anim.*
 import kotlinx.android.synthetic.main.app_bar_layout.*
+import android.view.animation.BounceInterpolator
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.transition.TransitionManager
+import android.view.View
+import com.arvifox.arvi.R
 
 class AnimActivity : AppCompatActivity() {
 
@@ -25,6 +31,7 @@ class AnimActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val va = ValueAnimator.ofFloat(0f, 100f)
+//        val va = AnimatorInflater.loadAnimator(this, R.animator.sometext) as ValueAnimator
         va.repeatCount = 2
         va.interpolator = AccelerateDecelerateInterpolator()
         va.duration = 700
@@ -33,5 +40,25 @@ class AnimActivity : AppCompatActivity() {
             tvSome1.rotationX = pro
         }
         va.start()
+
+        btnTrans.setOnClickListener(object : View.OnClickListener {
+            var vis: Boolean = false
+            @SuppressLint("NewApi")
+            override fun onClick(v: View?) {
+                TransitionManager.beginDelayedTransition(llTransition)
+                vis = !vis
+                tvTrans.visibility = if (vis) View.VISIBLE else View.GONE
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        btnSome.setOnClickListener {
+            val buttonAnimator = ObjectAnimator.ofFloat(btnSome, "translationX", 0f, 400f)
+            buttonAnimator.duration = 3000
+            buttonAnimator.interpolator = BounceInterpolator()
+            buttonAnimator.start()
+        }
     }
 }
