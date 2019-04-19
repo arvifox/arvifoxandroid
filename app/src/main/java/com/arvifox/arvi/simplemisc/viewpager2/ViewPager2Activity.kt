a61929a452aa31cec8e32d76745655c0d3dd72a6
arvifox <arvifox@hotmail.com>
2019-04-19 17:08:45 +0300

    viewpager2

--- app/src/main/java/com/arvifox/arvi/simplemisc/viewpager2/ViewPager2Activity.kt
+++ app/src/main/java/com/arvifox/arvi/simplemisc/viewpager2/ViewPager2Activity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.arvifox.arvi.R
 import kotlinx.android.synthetic.main.activity_view_pager2.*
 import kotlinx.android.synthetic.main.app_bar_layout.*
@@ -13,6 +14,20 @@ class ViewPager2Activity : AppCompatActivity() {
     companion object {
         fun newIntent(c: Context): Intent {
             return Intent(c, ViewPager2Activity::class.java)
+        }
+    }
+
+    val pageCallback = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageScrollStateChanged(state: Int) {
+            super.onPageScrollStateChanged(state)
+        }
+
+        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+        }
+
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
         }
     }
 
@@ -26,5 +41,15 @@ class ViewPager2Activity : AppCompatActivity() {
 //        vpViewpager2.orientation = ViewPager2.ORIENTATION_VERTICAL
 
         vpViewpager2.adapter = ViewPagerFragmentStateAdapter(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        vpViewpager2.registerOnPageChangeCallback(pageCallback)
+    }
+
+    override fun onPause() {
+        vpViewpager2.unregisterOnPageChangeCallback(pageCallback)
+        super.onPause()
     }
 }
