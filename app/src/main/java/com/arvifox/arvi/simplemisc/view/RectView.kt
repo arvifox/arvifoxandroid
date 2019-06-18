a7ccf0b149ac5430ff6ecedee50ae47b14b205b0
arvifox <arvifox@hotmail.com>
2019-06-18 17:43:36 +0200

    custom style theme attrs

---
+++ app/src/main/java/com/arvifox/arvi/simplemisc/view/RectView.kt
@@ -0,0 +1,43 @@
+package com.arvifox.arvi.simplemisc.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import com.arvifox.arvi.R
+
+class RectView @JvmOverloads constructor(
+        context: Context,
+        attrSet: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : View(context, attrSet, defStyleAttr) {
+
+    private val paint = Paint().apply {
+        color = Color.BLUE
+    }
+
+    init {
+        val typedArray = context.theme.obtainStyledAttributes(
+                attrSet,
+                R.styleable.RectView,
+                0,
+                0
+        )
+        try {
+            paint.color = typedArray.getColor(
+                    R.styleable.RectView_rectColor,
+                    Color.BLUE
+            )
+        } finally {
+            typedArray.recycle()
+        }
+    }
+
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
+    }
+}
