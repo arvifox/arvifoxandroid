cd77c54e47cc4e6d31f63cf848ddd1b0b3b4e930
arvifox <arvifox@hotmail.com>
2019-07-22 18:03:02 +0300

    coordinator layout:

--- app/src/main/java/com/arvifox/arvi/simplemisc/misc2/fragment3/Misc2Fragment31.kt
+++ app/src/main/java/com/arvifox/arvi/simplemisc/misc2/fragment3/Misc2Fragment31.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.arvifox.arvi.R
-import kotlinx.android.synthetic.main.fragment_3_1misc2.*
 
 class Misc2Fragment31 : Fragment() {
 
@@ -22,6 +21,6 @@ class Misc2Fragment31 : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        scrollcoordinator.scrollableView = scrollview
+//        scrollcoordinator.scrollableView = scrollview
     }
 }
