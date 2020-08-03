a21801a155a72557c18d017527d3a1ff7243965b
arvifox <arvifox@hotmail.com>
2020-08-03 09:22:38 +0300

    cae

---
+++ app/src/test/java/com/arvifox/arvi/kotlinpu/CorouTest.kt
@@ -0,0 +1,14 @@
+package com.arvifox.arvi.kotlinpu
+
+import com.arvifox.arvi.domain.corou.CoroutExce
+import org.junit.Assert
+import org.junit.Test
+
+class CorouTest {
+
+    @Test
+    fun test01() {
+        CoroutExce.qwer02()
+        Assert.assertEquals(true, true)
+    }
+}
