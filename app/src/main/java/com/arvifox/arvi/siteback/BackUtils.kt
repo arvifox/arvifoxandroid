469f058a35def41e7f3407425a257505b1b4fb08
Arvi Fox <arvifox@arvifox.com>
2022-03-11 00:20:50 +0300

    download url

--- app/src/main/java/com/arvifox/arvi/siteback/BackUtils.kt
+++ app/src/main/java/com/arvifox/arvi/siteback/BackUtils.kt
@@ -7,17 +7,34 @@ import android.os.Looper
 import com.arvifox.arvi.BuildConfig
 import com.arvifox.arvi.utils.BaseStorage
 import okhttp3.*
+import okio.buffer
+import okio.sink
+import java.io.File
 import java.io.IOException
 
 object BackUtils {
 
+    fun download(url: String, dest: File) {
+        OkHttpClient()
+            .newCall(Request.Builder().url(url).get().build())
+            .execute()
+            .use { response ->
+                val body = checkNotNull(response.body)
+                dest.sink().buffer().use { sink ->
+                    sink.writeAll(body.source())
+                }
+            }
+    }
+
     fun sendToken(token: String, con: Context) {
         val client = OkHttpClient()
         val request = Request.Builder()
-                .url(BuildConfig.ARVI_API_URL + "adddevice.php?id=" + token +
+            .url(
+                BuildConfig.ARVI_API_URL + "adddevice.php?id=" + token +
                         "&tel=" + Build.MANUFACTURER.replace(" ", "", true) +
-                        "_" + Build.MODEL.replace(" ", "", true))
-                .build()
+                        "_" + Build.MODEL.replace(" ", "", true)
+            )
+            .build()
         val han = Handler(Looper.getMainLooper())
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
