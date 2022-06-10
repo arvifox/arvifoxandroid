ba19e1251a9e689233a831895bb3622f78b07b0e
arvifox <arvifox@hotmail.com>
2022-06-10 15:49:47 +0300

    test compose

---
+++ FullTestCompose/app/src/main/java/com/arvifox/fulltestcompose/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.arvifox.fulltestcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.arvifox.fulltestcompose.ui.theme.FullTestComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FullTestComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", modifier = Modifier.fillMaxHeight(), textAlign = TextAlign.Center)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FullTestComposeTheme {
+        Button(onClick = { /*TODO*/ }, contentPadding = PaddingValues(0.dp), modifier = Modifier.height(28.dp)) {
+
+            Greeting(name = "check")
+        }
+    }
+}
