e4223ee0085d23aef975dc6e907e7e61eea3d4a4
arvifox <arvifox@hotmail.com>
2023-12-06 16:35:28 +0300

    configuration resolution gradle

--- arvifoxandroid/app/build.gradle.kts
+++ arvifoxandroid/app/build.gradle.kts
@@ -47,6 +47,13 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    configurations.all {
+        resolutionStrategy.eachDependency {
+            when {
+                requested.name == "core-ptx" -> useVersion("77.88.99")
+            }
+        }
+    }
 }
 
 dependencies {
