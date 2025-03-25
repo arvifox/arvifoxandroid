1aedeeb30a5bf683b8f90d44f8508f32c806d782
arvifox <arvifox@hotmail.com>
2025-03-25 14:56:40 +0300

    corocha

--- arvifoxandroid/app/src/main/java/com/arvifox/arvi/domain/corou/CoroChannel.kt
+++ arvifoxandroid/app/src/main/java/com/arvifox/arvi/domain/corou/CoroChannel.kt
@@ -1,9 +1,14 @@
 package com.arvifox.arvi.domain.corou
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 object CoroChannel {
@@ -11,9 +16,17 @@ object CoroChannel {
     suspend fun sendInt(a: Int) {
         cha.send(a)
     }
+
     suspend fun consumeInt() {
         for (a in cha) {
             println("channel int=$a")
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    suspend fun asd() = coroutineScope {
+        val ccc: ReceiveChannel<Long> = produce {
+            send(123L)
         }
     }
 
@@ -29,6 +42,9 @@ object CoroChannel {
         }
 
         channelFlow {
+            launch {
+
+            }
             withContext(Dispatchers.IO) {
                 send(123)
             }
