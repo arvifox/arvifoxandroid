4fbb286ed3788de374fe10a3b830d2e99d58bd81
Arvi Fox <arvifox@hotmail.com>
2025-07-22 23:34:38 +0300

    to ka ava

---
+++ starttoca/app/src/main/java/com/arvifox/starttoca/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.arvifox.starttoca
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.arvifox.starttoca.ui.theme.StartTocaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StartTocaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Icon(
+                            painter = painterResource(R.mipmap.ic_launcher),
+                            contentDescription = null,
+                            tint = Color.Unspecified,
+                            modifier = Modifier
+                                .fillMaxWidth(0.5f)
+                                .aspectRatio(1f)
+                                .clickable(
+                                    enabled = true,
+                                    onClick = {
+                                        val pn =
+                                            packageManager.getLaunchIntentForPackage("com.tocaboca.tocalifeworld")
+                                        pn?.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                                        startActivity(pn)
+                                    },
+                                ),
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
